4f9045ef5ab59b85e2ca1afd0b87a533e49237d1
David Balishyan <davidbalishyan12@gmail.com>
2025-08-06 21:37:54 +0400

    Added: some bash scripts

---
+++ zsh/setup-zsh.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+# ------------------------------------------------------------------------------------------------
+#                           A bash script to setup ZSH with ohMyZsh and p10k
+# Repo: https://github.com/DavidBalishyan/dotfiles
+# Creator: https://github.com/DavidBalishyan
+# ------------------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------------------
+#                                         Getting the resources
+curl https://example.com/ >~/.zshrc
+git clone # ohMyZsh + p10k
+# ------------------------------------------------------------------------------------------------
+
+# Restart Zsh
+exec zsh
