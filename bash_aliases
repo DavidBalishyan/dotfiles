6fc13de4f12ca7c881de7d4890d1b0a4b1c28f64
DavidBalishyan <davidbalishyan12@gmail.com>
2025-09-19 00:40:44 -0400

    Added: bash config for systems i don't wanna install zsh to

---
+++ bash_aliases
@@ -0,0 +1,4 @@
+alias ll="ls -l"
+alias la="ls -A"
+alias l="ls -lha"
+alias reload="exec bash"
---
+++ setup_bash.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ln -s "$(pwd)/bashrc" "$HOME/.bashrc"
+ln -s "$(pwd)/bash_aliases" "$HOME/.bash_aliases"
