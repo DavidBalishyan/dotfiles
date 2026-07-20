8fa3b18d942110c6851016a2724e084a5c12f106
David Balishyan <davidbalishyan12@gmail.com>
2026-07-20 17:13:53 +0400

    Trying out kitty, but ima stick to st

--- setup.sh
+++ setup.sh
@@ -92,6 +92,7 @@ link_if_installed foot "$repo_root/foot.ini" "$HOME/.config/foot/foot.ini" "foot
 link_if_installed yazi "$repo_root/yazi" "$HOME/.config/yazi" "yazi"
 link_if_installed bash "$repo_root/bashrc" "$HOME/.bashrc" "bash"
 link_if_installed cboomer "$repo_root/config.boom" "$HOME/.config/cboomer/config" "cboomer"
+link_if_installed kitty "$repo_root/kitty.conf" "$HOME/.config/kitty/kitty.conf" "kitty"
 
 # Xinitrc is a special case since it's only relevant if the user is running Xorg
 if [ -n "$DISPLAY" ] && [ -x "$(command -v startx)" ]; then
